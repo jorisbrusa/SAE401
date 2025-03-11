2ed91fa97ba68610628d5ad4a8dc4a4dc0cd8996
Markus-Otto <tommanlay4@gmail.com>
2025-03-11 10:21:08 +0100

    v4  ajout accueil

--- EasyToDrive/src/app/app.component.ts
+++ EasyToDrive/src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { routes } from './app.routes';
+import { MonEspaceConexionComponent } from './component/mon-espace-conexion/mon-espace-conexion.component';
+import { MonEspaceDashboardComponent } from './component/mon-espace-dashboard/mon-espace-dashboard.component';
+import { AccueilComponent } from './component/accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MonEspaceConexionComponent, MonEspaceDashboardComponent, AccueilComponent, routes],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
