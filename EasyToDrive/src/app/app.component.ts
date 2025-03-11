b8e0107b726c5e0050deb777f0e697003cd50964
Markus-Otto <tommanlay4@gmail.com>
2025-03-11 09:17:02 +0100

    V2

--- EasyToDrive/src/app/app.component.ts
+++ EasyToDrive/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
