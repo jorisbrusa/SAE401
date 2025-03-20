d3263e2ff1487655b0dfc3742ec96d68fbe6f053
Markus-Otto <tommanlay4@gmail.com>
2025-03-20 12:43:51 +0100

    V 34.1 ajout securiter connexion

--- EasyToDrive/src/app/guards/auth.guard.ts
+++ EasyToDrive/src/app/guards/auth.guard.ts
@@ -1,22 +1,21 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
-  private router = inject(Router);
+  
+  constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const adminId = localStorage.getItem('adminID'); 
-    const eleveId = localStorage.getItem('eleveID');
-
-    if (adminId || eleveId) {
-      return true;
+    const eleveID = localStorage.getItem('eleveID');
+    const adminID = localStorage.getItem('adminID');
+    
+    if (eleveID || adminID) {
+      return true; // L'utilisateur est authentifié
     } else {
-      console.warn("Accès refusé - Redirection vers /mon-espace");
-      this.router.navigate(['/mon-espace']);
+      this.router.navigate(['/mon-espace']); // Redirection vers la connexion
       return false;
     }
   }
